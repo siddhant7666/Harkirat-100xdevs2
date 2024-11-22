26f9b0511883056aa4c96fdba43ce6aac287fdd5
Siddhant <dearsidd2017@gmail.com>
2024-11-22 12:47:29 +0530

    variables, dataypes, if-else in js

--- Week 1/main.js
+++ Week 1/main.js
@@ -1,2 +1,28 @@
-console.log("Hello world!");
-console.log("Hello world! again!");
+// console.log("Hello world!");
+// console.log("Hello world! again!");
+
+
+// var(deprecated), let, const -> variables
+ let a = 1;
+ a = 2;
+ console.log(a);
+
+ //datatypes - string, boolean, number
+
+ let first_name = "Siddhant"; //string
+ let age = 18;  //number 
+ let isMarried = true; //boolean
+ console.log("This person name is: " +first_name + " His age is " + age + " is he married?" + isMarried);
+
+
+ if(isMarried == true)
+ {
+    console.log("This person is married:" +first_name);
+
+ }
+ else
+ {
+    console.log("This person is not married.");
+ }
+
+ 
