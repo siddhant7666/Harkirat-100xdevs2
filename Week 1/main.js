d4b972b7038efacb753b492db09573ec3130255b
Siddhant <dearsidd2017@gmail.com>
2024-11-22 16:18:42 +0530

    odd - even js

--- Week 1/main.js
+++ Week 1/main.js
@@ -34,3 +34,25 @@
     sum += i;
  }
  console.log("Sum is: " + sum);
+
+
+// Arrays:
+const personArray = ["Sidd", "John", "Alice"];
+ console.log(personArray);
+ console.log(personArray[0]);
+ console.log(personArray[1]);
+ console.log(personArray[2]);
+
+ // Write a program that prints all the even numbers in an array
+ const Numberarray = [21, 22, 23, 24, 25, 26, 27, 28, 29];
+ for(let i =0; i<Numberarray.length; i++)
+ {
+   if(Numberarray[i]%2==0)
+   {
+      console.log(Numberarray[i] + " is even");
+   }
+   else
+   {
+      console.log(Numberarray[i] + " is odd");
+   }
+ }
